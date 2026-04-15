11248c3ce13b3642c3487d414763a9fd98508926
Abdulaziz Altwaijri <aziz@Abdulazizs-MacBook.local>
2026-04-15 08:45:09 +0300

    TODO3

--- 7-2-restful-api/server/server.js
+++ 7-2-restful-api/server/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
-// import dotenv and load environment variables from .env
-
+dotenv.config();
 
 import { connectDB } from "./db.js";
 import { Song } from "./models/song.model.js";
@@ -10,19 +10,7 @@ import { Song } from "./models/song.model.js";
 const app = express();
 const PORT = process.env.PORT || 5174;
 
-app.use(cors());              
+app.use(cors());
 app.use(express.json());
 
 await connectDB(process.env.MONGO_URL);
-
-// api/songs (Read all songs)
-
-
-// api/songs (Insert song)
-
-// /api/songs/:id (Update song)
-
-
-// /api/songs/:id (Delete song)
-
-app.listen(PORT, () => console.log(`API running on http://localhost:${PORT}`));
